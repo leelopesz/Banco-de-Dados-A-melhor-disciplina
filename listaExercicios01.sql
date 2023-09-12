b60206e9bebaddff5d89f0f270b9f14334bd5841
leelopesz <99299548+leelopesz@users.noreply.github.com>
2023-09-11 23:27:23 -0300

    Exercício 04

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -8,3 +8,8 @@ where nascimento < '1900-01-01';
 -- Exercício 03
 select * from livros 
 where autor_id = 1;
+
+-- Exercício 04
+select distinct nome from alunos
+inner join matriculas on alunos.id = matriculas.aluno_id
+where matriculas.curso = 'Engenharia de Software'; 
